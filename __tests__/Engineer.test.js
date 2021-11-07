cd4d62b3a80469a5c34c2ae97374d9bc2a20db97
DanWilk <dcwilkinson@wilktech.com>
2021-11-07 12:44:47 -0500

    created Engineer object

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,21 @@
+const Engineer = require('../lib/Engineer');
 
-const Employee = require('../lib/Employee');
+test('creates engineer object and imports emplyee data', () => {
+    const engineer = new Engineer('Dave Heck', 1, 'none@none.com','DanWilk');
 
-test('creates engineer object and imports emplyee data', ())
+    expect(engineer.name).toBe('Dave Heck');
+    expect(engineer.email).toBe('none@none.com');
+    expect(engineer.GitHub).toBe('DanWilk');
+});
+
+test('checks if role has been overridden', () => {
+    const engineer = new Engineer('Dave Heck', 1, 'none@none.com','DanWilk');
+    
+    expect(engineer.getRole()).toBe('Engineer');
+});
+
+test('returns Github accout', () => {
+    const engineer = new Engineer('Dave Heck', 1, 'none@none.com','DanWilk');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.GitHub));
+});
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,4 +1,3 @@
-const Employee = require('../lib/Employee');
 const Mananger = require('../lib/Manager');
 
 test('creates manager object and imports employee data', () => {
@@ -7,11 +6,11 @@ test('creates manager object and imports employee data', () => {
     expect(manager.name).toBe('Dave Heck');
     expect(manager.email).toBe('none@none.com');
     expect(manager.officeNumber).toEqual(expect.any(String));
-})
+});
 
 test('checks if role has been changed to manager', () => {
     const manager = new Mananger( 'Dave Heck', 1, 'none@none.com','434-444-5454');
     
     expect(manager.getRole()).toBe('Manager');
-})
+});
 
